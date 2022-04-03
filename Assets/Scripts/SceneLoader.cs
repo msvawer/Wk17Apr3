d2ca34aba312a3e4597887372906632471765264
MichVawer <michelle.vawer@icloud.com>
2022-04-03 14:49:58 -0400

    Setting up xr rig

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -125,4 +125,5 @@ public class SceneLoader : Singleton<SceneLoader>
     // }
 
 }
+
 
